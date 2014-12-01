e0d540ca86d93c0f816ac56430dc2c03d56f0e38
Elais Jackson <machinebrainx@gmail.com>
2014-12-01 11:20:02 -0600

    some changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -77,12 +77,12 @@
  '(ring-bell-function (quote ignore) t)
  '(tool-bar-mode nil))
 
-(custom-set-faces
+;;(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "M+ 2m regular" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+ ;;'(default ((t (:family "M+ 2m regular" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
 
 (put 'narrow-to-region 'disabled nil)
 
--- .emacs.d/subconfigs/init-org.el
+++ .emacs.d/subconfigs/init-org.el
@@ -1,13 +1,17 @@
 (require 'org)
 
 (if (eq system-type 'windows-nt)
-    (setq org-directory "C:/ilionData/Users/juanjo.alvarez/Documents/Dropbox/Org")
+    (setq org-directory "C:/Users/elais_000/Documents/Dropbox/org")
   (setq org-directory "~/Dropbox/Org"))
 
 (if (eq system-type 'windows-nt)
-    (setq org-mobile-directory "C:/ilionData/Users/juanjo.alvarez/Documents/Dropbox/MobileOrg")
+    (setq org-mobile-directory "C:Users/elais_000/Documents/Dropbox/MobileOrg")
   (setq org-mobile-directory "~/Dropbox/MobileOrg"))
 
+(if (eq system-type 'darwin)
+	(setq org-directory "~/Dropbox/org")
+  (setq org-directory "~/Dropbox/org"))
+  
 (setq org-mobile-inbox-for-pull (concat org-directory "/flagged.org"))
 
 (provide 'init-org)
