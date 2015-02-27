080a5a34085b4a105b9f6c86932239f828dcb260
Elais Jackson <machinebrainx@gmail.com>
2015-02-27 09:52:04 -0600

    removed cask

--- .emacs.d/.cask/24.4.1/elpa/evil-search-highlight-persist-20150107.4/evil-search-highlight-persist-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-search-highlight-persist" "20150107.4" "Persistent highlights after search" '((highlight "0")))
--- .emacs.d/.cask/24.4.1/elpa/noctilux-theme-20140406.2/noctilux-theme-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; noctilux-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "noctilux-definitions" "noctilux-definitions.el"
-;;;;;;  (21725 32724 0 0))
-;;; Generated autoloads from noctilux-definitions.el
-
-(when (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("noctilux-theme-pkg.el" "noctilux-theme.el")
-;;;;;;  (21725 32724 735062 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; noctilux-theme-autoloads.el ends here
